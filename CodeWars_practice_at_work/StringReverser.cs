5e38fad21635e4a4bc683262b5f6b796092d1a20
Molnar Laszlo <laszlo.molnar25@gmail.com>
2020-01-26 18:45:42 +0100

    Add reverser

---
+++ CodeWars_practice_at_work/StringReverser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    public static class StringReverser
+    {
+        public static string ReverseString(string input)
+        {
+            string output = String.Empty;
+            var charList = input.ToList();
+            charList.Reverse();
+            charList.ForEach(e => output += e);
+            return output;
+        }
+    }
+}
